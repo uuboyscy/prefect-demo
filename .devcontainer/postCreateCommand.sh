78ede4ef76d6ed1c63cbf6a542ffad238790e31d
uuboyscy <aegis12321@gmail.com>
2024-07-29 15:33:50 +0800

    build: Devcontainer configuration

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -4,10 +4,12 @@ set -x
 sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="xiong-chiamiov-plus"/' /root/.zshrc \
 && sed -i '/HIST_STAMPS="mm\/dd\/yyyy"/s/^#*//;s/mm\/dd\/yyyy/yyyy-mm-dd/' /root/.zshrc
 
-# cp /root/.gitconfig_mounted /root/.gitconfig \
-# && cp -r /root/.ssh_mounted/* /root/.ssh/ \
-# && chmod 400 /root/.ssh/id_rsa \
-# && chmod 755 /root/.ssh/config
+cp /root/.gitconfig_mounted /root/.gitconfig \
+&& cp -r /root/.ssh_mounted/* /root/.ssh/ \
+&& ls /root/.ssh/* | grep -v pub | xargs chmod 400
+
+chmod 755 /root/.ssh/config
+chmod 755 /root/.ssh/known_hosts
 
 git config --global --add safe.directory /workspaces/*
 
