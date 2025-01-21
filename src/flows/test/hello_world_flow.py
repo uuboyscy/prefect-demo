d0a738b783920f844fa299c408bb76ee871f526a
uuboyscy <aegis12321@gmail.com>
2025-01-21 16:37:46 +0800

    feat: :test_tube: Testing

--- src/flows/test/hello_world_flow.py
+++ src/flows/test/hello_world_flow.py
@@ -1,8 +1,10 @@
+"""Quick start."""
+
 from prefect import flow
 
 
 @flow(log_prints=True)
-def hello_world_flow(name: str = "world", goodbye: bool = False):
+def hello_world_flow(name: str = "world", goodbye: bool = False) -> None:
     print(f"Hello {name} from Prefect! 🤗")
 
     if goodbye:
@@ -10,16 +12,39 @@ def hello_world_flow(name: str = "world", goodbye: bool = False):
 
 
 if __name__ == "__main__":
-    from prefect_github import GitHubRepository
 
+    """
+    Run flow directly, just like Python functions. And the process can also be monitored on UI.
+    """
+    # hello_world_flow()
+    # hello_world_flow(name="Ted", goodbye=True)
+
+    """
+    Start a temporary local server of worker to execute flows submitted from Prefect cloud.
+    It comes with a development named "my-first-deployment" which can be found on UI.
+    """
+    hello_world_flow.serve(
+        name="my-first-deployment",  # Deployment name. It create a temporary deployment.
+        tags=["onboarding"],  # Filtering when searching on UI.
+        parameters={
+            "goodbye": True
+        },  # Overwrite default parameters defined on hello_world_flow. Only for this deployment.
+        interval=60,  # Like crontab, "* * * * *"
+    )
+
+    """
+    Deploy a flow to Prefect cloud.
+    """
+    from prefect_github import GitHubRepository
     hello_world_flow.from_source(
-        source=GitHubRepository.load("github-prefect-demo"),
-        entrypoint="src/flow/test/hello_world_flow_flow.py:hello_world_flow",
+        source=GitHubRepository.load("github-repository-uuboyscy"),
+        entrypoint="src/flows/sample/hello_world_flow.py:hello_world_flow",
     ).deploy(
-        name="test-deploy",
-        tags=["test", "project_1"],
-        work_pool_name="test-subproc",
-        job_variables=dict(pull_policy="Never"),
-        # parameters=dict(name="Marvin"),
-        cron="1 * * * *"
+        name="main",  # Deployment name.
+        tags=["dev", "sample"],
+        work_pool_name="dev-subproc",  # A worker to submit the deployment.
+        parameters=dict(
+            name="Marvin"
+        ),  # Overwrite default parameters defined on hello_world_flow. Only for this deployment.
+        cron="*/15 * * * *",  # Crontab for this deployment.
     )
