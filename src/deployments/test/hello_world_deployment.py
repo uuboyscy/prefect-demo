418eb104d646942bb94e6dec7eea93a3dea80423
uuboyscy <aegis12321@gmail.com>
2025-04-25 23:03:18 +0800

    refactor: Sample flow

--- src/deployments/test/hello_world_deployment.py
+++ src/deployments/test/hello_world_deployment.py
@@ -6,11 +6,11 @@ if __name__ == "__main__":
 
     hello_world_flow.from_source(
         source=GitHubRepository.load("github-repository-uuboyscy"),
-        entrypoint="src/flow/test/hello_world_flow_flow.py:hello_world_flow",
+        entrypoint="src/flows/test/hello_world_flow.py:hello_world_flow",
     ).deploy(
         name="test-deploy",
         tags=["test", "project_1"],
-        work_pool_name="test-subproc",
+        work_pool_name="serverless",
         job_variables=dict(pull_policy="Never"),
         # parameters=dict(name="Marvin"),
         cron="1 * * * *"
