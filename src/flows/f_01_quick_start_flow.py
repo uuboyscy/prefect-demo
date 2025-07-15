4f9d8def5e6c8a4266318e465c13f202b74b2238
uuboyscy <aegis12321@gmail.com>
2025-07-15 22:47:02 +0800

    refactor: :lipstick: Formatted and removed unused code

--- src/flows/f_01_quick_start_flow.py
+++ src/flows/f_01_quick_start_flow.py
@@ -9,15 +9,18 @@ def e_data_source_1() -> pd.DataFrame:
     print("Getting df1.")
     return pd.DataFrame(data=[[1], [2]], columns=["col"])
 
+
 @task
 def e_data_source_2() -> pd.DataFrame:
     print("Getting df2.")
     return pd.DataFrame(data=[[3], [4]], columns=["col"])
 
+
 @task
 def t_concat(df1: pd.DataFrame, df2: pd.DataFrame) -> pd.DataFrame:
     print("Concating df1 and df2.")
     return pd.concat([df1, df2]).reset_index(drop=True)
+
 
 @task
 def l_db1(df: pd.DataFrame) -> None:
@@ -25,11 +28,13 @@ def l_db1(df: pd.DataFrame) -> None:
     print(df)
     print("===============")
 
+
 @task
 def l_db2(df: pd.DataFrame) -> None:
     print("Loading df to db2.")
     print(df)
     print("===============")
+
 
 @flow(name=generate_flow_name())
 def f_01_quick_start_flow() -> None:
@@ -39,19 +44,6 @@ def f_01_quick_start_flow() -> None:
     l_db1(df)
     l_db2(df)
 
+
 if __name__ == "__main__":
-    # f_01_quick_start_flow()
-    from prefect_github import GitHubRepository
-
-
-    f_01_quick_start_flow.from_source(
-        source=GitHubRepository.load("github-prefect-demo"),
-        entrypoint="src/flow/f_01_quick_start_flow.py:f_01_quick_start_flow",
-    ).deploy(
-        name="test-deploy",
-        tags=["test", "project_1"],
-        work_pool_name="test-docker",
-        job_variables=dict(pull_policy="Never"),
-        # parameters=dict(name="Marvin"),
-        cron="1 * * * *"
-    )
+    f_01_quick_start_flow()
--- src/flows/f_04_forloop_submit_flow.py
+++ src/flows/f_04_forloop_submit_flow.py
@@ -1,4 +1,3 @@
-import pandas as pd
 from prefect import flow, task
 
 from utils.prefect_utility import generate_flow_name
@@ -8,15 +7,18 @@ from utils.prefect_utility import generate_flow_name
 def generate_some_str() -> str:
     return "HELLO"
 
+
 @task
 def do_something(some_str: str) -> list[str]:
     return list(some_str)
+
 
 @task
 def print_something_separately(something: str | list) -> None:
     print("======")
     print(something)
     print("======")
+
 
 @flow(name=generate_flow_name())
 def f_04_forloop_submit_flow() -> None:
@@ -25,18 +27,6 @@ def f_04_forloop_submit_flow() -> None:
     for each_str in result:
         print_something_separately.submit(each_str)
 
-if __name__ == "__main__":
-    from prefect_github import GitHubRepository
-    # f_04_forloop_submit_flow()
 
-    f_04_forloop_submit_flow.from_source(
-        source=GitHubRepository.load("github-prefect-demo"),
-        entrypoint="src/flow/f_04_forloop_submit_flow.py:f_04_forloop_submit_flow",
-    ).deploy(
-        name="test-deploy",
-        tags=["test", "project_4"],
-        work_pool_name="test-docker",
-        job_variables=dict(pull_policy="Never"),
-        # parameters=dict(name="Marvin"),
-        cron="*/30 0-8,9-15,16-23 * * *"
-    )
+if __name__ == "__main__":
+    f_04_forloop_submit_flow()
